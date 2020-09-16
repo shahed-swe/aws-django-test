a9e7eabd7da7bc44c3c966a0d06eda0eac2e0f5e
Shahed Talukder <shahedtalukder51@gmail.com>
2020-09-16 20:34:51 +0600

    new commit

--- new_app/urls.py
+++ new_app/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import home
+from main.views import home, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home),
+    path('about/',about),
 ]
